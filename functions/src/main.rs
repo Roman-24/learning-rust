7f0978cf449cf182f7d56a89e01ba7c71df70745
Roman-24 <xbitarovsky@stuba.sk>
2024-07-30 23:22:12 +0200

    functions return value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
     print_labeled_measurement(5, 'h');
+    test_section();
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn test_section() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
