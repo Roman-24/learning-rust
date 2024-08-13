8629963dd0e426b436dd8b38d8e1ffdf9a8c46c2
Roman-24 <xbitarovsky@stuba.sk>
2024-08-13 15:03:46 +0200

    ownership learning references and borrowing - mutabownership learning references and borrowing - mutable reference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -4,8 +4,16 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{s1}' is {len}.");
+
+    let mut s2 = String::from("hello");
+    change(&mut s2);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+    println!("{}", some_string)
 }
