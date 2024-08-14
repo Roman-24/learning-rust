72940f3d1c2fa92628f561854348497cd667d4ab
Roman-24 <xbitarovsky@stuba.sk>
2024-08-14 16:16:13 +0200

    struct

---
+++ structs/src/main.rs
@@ -0,0 +1,26 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let mut user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+
+    user1.email = String::from("anotheremail@example.com");
+
+    println!("User email is: {}", user1.email);
+}
