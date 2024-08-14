1210662f3ee19b21e557c93ad86bfe8e488a018e
Roman-24 <xbitarovsky@stuba.sk>
2024-08-14 16:28:06 +0200

    tuple structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,33 +1,9 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    let mut user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123"),
-    );
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 10, -20);
 
-    user1.email = String::from("anotheremail@example.com");
-
-    println!("User1 email is: {}", user1.email);
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    println!("User2 email is: {}", user2.email);
+    println!("x: {}, y: {}, z: {}", origin.0, origin.1, origin.2);
 }
