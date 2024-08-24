dce5759f476e060380ffb02dfc33c62a60dc0141
roman <xromanb@proton.me>
2024-08-24 19:13:08 +0200

    custom smart pointer

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,4 +1,22 @@
+#[derive(Debug)]
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
-    let b = Box::new(5);
-    println!("b = {b}");
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created:");
+    println!("{:?}", &c);
+    println!("{:?}", &d);
 }
