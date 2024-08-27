9bc645d1da5e58a5d225e3f7c4a870fb8e38cefb
roman <xromanb@proton.me>
2024-08-27 17:39:42 +0200

    channel for multiple threads

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -3,7 +3,9 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let (tx_1, rx) = mpsc::channel();
+
+    let tx_2 = tx_1.clone();
 
     thread::spawn(move || {
         let vals = vec![
@@ -14,14 +16,36 @@ fn main() {
         ];
 
         for val in vals {
-            println!("Thread: sending message..");
-            tx.send(val).unwrap();
-            println!("Thread: i am going to sleep...");
+            println!("Thread 1: sending message..");
+            tx_1.send(val).unwrap();
+            println!("Thread 1: i am going to sleep...");
             thread::sleep(Duration::from_secs(2));
         }
     });
 
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            println!("Thread 2: sending message..");
+            tx_2.send(val).unwrap();
+            println!("Thread 2: i am going to sleep...");
+            thread::sleep(Duration::from_secs(2));
+        }
+    });
+
+    let mut count = 0;
     for received in rx {
         println!("Main: i just received message: {received}");
+        count += 1;
+        if count == 2 {
+            println!();
+            count = 0;
+        }
     }
 }
