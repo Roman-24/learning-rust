0d46bca1668ca3455046ab21a1870eab12efd8b3
roman <xromanb@proton.me>
2024-08-27 17:30:25 +0200

    channel send and receive w sleep

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -1,14 +1,27 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            println!("Thread: sending message..");
+            tx.send(val).unwrap();
+            println!("Thread: i am going to sleep...");
+            thread::sleep(Duration::from_secs(2));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {received}");
+    for received in rx {
+        println!("Main: i just received message: {received}");
+    }
 }
