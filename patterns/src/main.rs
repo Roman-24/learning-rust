8d2922602e3c32a3746d693970eea807234557da
roman <xromanb@proton.me>
2024-08-30 16:12:36 +0200

    pattern syntax

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -36,6 +36,60 @@ fn main() {
     // A function with parameters that destructure a tuple
     let point = (3, 5);
     print_coordinates(&point);
+
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 1;
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // bindings
+    enum Message {
+        Hello { id: i32 },
+    }
+    let msg = Message::Hello { id: 10 };
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
