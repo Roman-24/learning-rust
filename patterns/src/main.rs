384d65fec4dc158813383bc2da00699fd173e12d
roman <xromanb@proton.me>
2024-08-30 11:16:10 +0200

    patterns

---
+++ patterns/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    // Mixing if let, else if, else if let, and else
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    // Using while let to loop over
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    while let Some(top) = stack.pop() {
+        println!("{top}");
+    }
+
+    // Using a pattern in a for loop to destructure a tuple
+    let v = vec!['a', 'b', 'c'];
+    for (index, value) in v.iter().enumerate() {
+        println!("{value} is at index {index}");
+    }
+
+    // A function with parameters that destructure a tuple
+    let point = (3, 5);
+    print_coordinates(&point);
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({x}, {y})");
+}
